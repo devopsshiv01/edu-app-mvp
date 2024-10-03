e04ba5a3f7dc81d43d7b9c3a8a286ef5b1bdacb2
deevopstechconsult <138079973+devopsshiv01@users.noreply.github.com>
2024-10-03 03:05:23 -0400

    Update app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Educational App MVP!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
